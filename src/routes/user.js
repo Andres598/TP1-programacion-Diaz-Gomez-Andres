81b8ba005075ba145fe55174fb73fd7b2e376b83
Andres598 <andresdiazgomez63@gmail.com>
2024-04-30 17:07:31 -0300

    corrección de llaves en routes/user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -24,7 +24,7 @@ router.get('/users/:id', (req,res)=>{
 router.put('/users/:id', (req,res)=>{
     const {id} = req.params;
     const {name,age,email} = req.body
-    userSchema.updateOne({_id:id},{$set: name,age,email}).then((data)=>res.json(data)).catch((error)=>res.json({message:error}));
+    userSchema.updateOne({_id:id},{$set: {name, age, email}}).then((data)=>res.json(data)).catch((error)=>res.json({message:error}));
 })
 
 //eliminar usuario
